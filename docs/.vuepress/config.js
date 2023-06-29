d48f3a3f8884aa4a6478c9c7a896301655f7ad8d
CHP <1768581208@qq.com>
2023-06-29 17:18:08 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,5 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
     title: '测试笔记',
     lang: 'zh-CN',
-    base:'/test-deploy-vuepress.vercel.app/'
+    // base:'/test-deploy-vuepress.vercel.app/'
 })
